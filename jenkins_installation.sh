1d1b898bb258394a44fbd43b26d3e2232b1955f2
Lalit Soni <lsoni3404@gmail.com>
2022-08-07 02:56:38 +0530

    updating files

---
+++ jenkins_installation.sh
@@ -0,0 +1,14 @@
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+sudo ufw allow 8080
+sudo ufw status
+sudo ufw allow OpenSSH
+sudo ufw enable
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
